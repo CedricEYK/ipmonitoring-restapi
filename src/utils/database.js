814b5efda0ff61d811cdb99223a9c49f37e96dc6
CedricEYK <34346892+CedricEYK@users.noreply.github.com>
2021-06-09 19:19:22 +0200

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const MONGODB_URI =
-  "mongodb+srv://cedDev:TLPE5k6p6tPkxEQM@cluster0.stbqe.mongodb.net/IPMonitoringDB?retryWrites=true&w=majority";
+  "";
 
 const mongooseConnect = (callback) => {
   mongoose
